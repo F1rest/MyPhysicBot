02720cafc07ce000ac06c6dd1b7818eedf910c8a
Sir Gay <tcd2004@mail.ru>
2018-12-15 15:36:01 +0300

    from school to home

--- run_bot2_3_1.py
+++ run_bot2_3_1.py
@@ -6,11 +6,6 @@ from config import *
 
 from config import TOKEN
 
-
-def write_msg(user_id, text):
-    vk_bot.method('messages.send', {'user_id': user_id, 'message': text})
-
-
 vk_bot = vk_api.VkApi(token=TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
 server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
@@ -18,6 +13,10 @@ print("Готов к работе")
 
 
 # +str(long_poll)}
+
+def write_msg(user_id, text):
+    vk_bot.method('messages.send', {'user_id': user_id, 'message': text})
+
 
 def write_msg_attach(user_id, text, att_url):
     vk_bot.method('messages.send',
@@ -44,11 +43,31 @@ while True:
               str(user_name[0]['last_name']) + ' написал(a) боту - ' + str(update[0][6]))
         text = update[0][6].lower()
 
-        if "привет" in str(text):
-            write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']))
+        # if 'привет' in str(text):
+        #     write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']))
+        #
+        # elif 'help' in str(text):
+        #     write_msg(user_id,
+        #               'Комманды: 1.Привет   2.Красивая картинка   3.Огород, бублик, зима, детская (любую из этих четырех)')
 
-        elif 'картинк' in update[0][6]:
+        if 'красив' in str(text):
             write_msg_attach(user_id,
-                             'вот тебе аниме xD',
-                             'photo-173254878_456239018')
+                             'Красивая картинка',
+                             'photo-175296561_456239017')
+        elif 'огород' in str(text):
+            write_msg_attach(user_id,
+                             'Лови',
+                             'video258838355_456239161')
+        elif 'бублик' in str(text):
+            write_msg_attach(user_id,
+                             'Держи',
+                             'video459897331_456239046')
+        elif 'зима' in str(text):
+            write_msg_attach(user_id,
+                             'Хватай',
+                             'video258838355_456239156')
+        elif 'детская' in str(text):
+            write_msg_attach(user_id,
+                             'Пока все, но я буду добавлять их)',
+                             'video-172678394_456239682')
     ts = long_poll['ts']
